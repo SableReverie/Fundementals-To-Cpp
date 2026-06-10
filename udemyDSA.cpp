ca5b000b421553422cafb5f192d718673142661a
Prince <princecultura10@gmail.com>
2026-06-10 17:43:24 +0800

    Added Drop Non-Dominants

--- udemyDSA.cpp
+++ udemyDSA.cpp
@@ -1,4 +1,4 @@
-// ============================ Big O: Worst Case ============================
+// ============================ Big O: O(n) Worst Case, Average Case, Best Case ============================
 /*
 #include <iostream>
 
@@ -17,6 +17,58 @@ int main() {
     printItems(10);
 
 }
+
+// The "1" is best case, the "4" is average case, the "7" is the worst case.
 */
 
-// ======================================= Big O: O(n) ============================================
+// ============================== Drop Constants =====================================
+// This function is n + n = 2n but the constant 2 will be dropped, so this will be O(n).
+/*
+#include <iostream>
+
+using namespace std;
+
+
+void printItems(int n) {
+    for (int i = 0; i < n; i++) {
+        cout << i << endl;
+    }
+
+    for (int j = 0; j < n; j++) {
+        cout << j << endl;
+    }
+}
+
+
+int main() { 
+
+    printItems(10);
+
+}
+*/
+
+// ================================== Big O: O(n^2) ==================================
+// n * n = n^2, but it's less efficient than O(n)
+/*
+#include <iostream>
+
+using namespace std;
+
+
+void printItems(int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << i << j << endl;
+        }
+    }
+}
+
+
+int main() { 
+
+    printItems(10);
+
+}
+*/
+
+// ============================== 
